1e2d0873df09531afdf60a7e4d170597a5682c63
Jung-min <kavin2971@gmail.com>
2023-02-16 10:39:04 +0900

    07
    07

--- day06/src/ex/Ex0005.java
+++ day06/src/ex/Ex0005.java
@@ -3,7 +3,41 @@ package ex;
 public class Ex0005 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+				 int [][]score = {{95, 80, 47},
+							      {75, 29, 30},
+							      {62, 76, 90},
+								  {83, 84, 79}};
+				int korSum = 0;
+				int engSum = 0;
+				int matSum = 0;
+				
+				 
+				 System.out.println("    국어   영어   수학   총점   평균 ");
+				 System.out.println("    ---------------------------");
+				 
+				 
+				 
+				 for (int i=0; i<score.length; i++) {
+					 int sum=0;
+					
+					 korSum +=score[i][0];
+					 engSum +=score[i][1];
+					 matSum +=score[i][2];
+					 
+				 
+					 					 
+					 for (int j=0; j<score[i].length; j++)	{	
+					 
+					 sum += score[i][j];
+					 
+				 System.out.printf("%6d", score[i][j]);
+				 
+				 }
+					 System.out.printf("%6d", sum);
+					 System.out.printf("%7.1f\n",(double)sum/3);
+ 
+				 }
 
 	}
 
